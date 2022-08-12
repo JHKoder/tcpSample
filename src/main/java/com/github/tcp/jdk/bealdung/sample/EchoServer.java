f5381bab7af54e0d1baaed67e0559ec963fcf5c4
oiNeh <kgh22522252@gmail.com>
2022-08-12 22:40:17 +0900

    :sparkles: loop server 추가

---
+++ src/main/java/com/github/tcp/jdk/bealdung/sample/EchoServer.java
@@ -0,0 +1,46 @@
+package com.github.tcp.jdk.bealdung.sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out = new PrintWriter(System.out);
+    private BufferedReader in;
+
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            if (".".equals(inputLine)) {
+                out.println("good bye");
+                break;
+            }
+            out.println(inputLine);
+        }
+    }
+
+    public void stop(String msg) throws IOException {
+        out.print(msg);
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        EchoServer server = new EchoServer();
+        server.start(4444);
+        server.stop("down");
+    }
+
+}
